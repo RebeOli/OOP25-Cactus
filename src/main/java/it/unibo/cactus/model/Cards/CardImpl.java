652206d8b787361b18d0e2a2b0dd352cd9b21392
Arianna Mondardini <arianna.mondardini@studio.unibo.it>
2026-03-05 15:40:36 +0100

    Class: CardImpl.java created

---
+++ src/main/java/it/unibo/cactus/model/Cards/CardImpl.java
@@ -0,0 +1,31 @@
+package it.unibo.cactus.model.Cards;
+
+import java.util.Optional;
+
+public class CardImpl implements Card{
+
+    @Override
+    public int getValue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getValue'");
+    }
+
+    @Override
+    public Suit getSuit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSuit'");
+    }
+
+    @Override
+    public int getScore() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getScore'");
+    }
+
+    @Override
+    public Optional<SpecialPower> getSpecialPower() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSpecialPower'");
+    }
+
+}
