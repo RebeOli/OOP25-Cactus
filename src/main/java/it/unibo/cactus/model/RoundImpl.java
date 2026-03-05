f7ee47de1123cc54178ac5ce4e26af363d903946
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-03-05 15:12:24 +0100

    RoundImpl.java created

---
+++ src/main/java/it/unibo/cactus/model/RoundImpl.java
@@ -0,0 +1,32 @@
+package it.unibo.cactus.model;
+
+import java.util.List;
+import java.util.Optional;
+
+public class RoundImpl implements Round{
+
+    @Override
+    public List<RoundAction> getAvailableActions() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAvailableActions'");
+    }
+
+    @Override
+    public boolean isLastRound() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isLastRound'");
+    }
+
+    @Override
+    public Optional<Card> getDrawnCard() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDrawnCard'");
+    }
+
+    @Override
+    public void execute(RoundAction action) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
