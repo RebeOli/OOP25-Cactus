2ee630743a3db4aee3097952e2acb80c2e3c0987
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2026-03-08 17:39:36 +0100

    Record GameResult.java implemented

--- src/main/java/it/unibo/cactus/model/Score/GameResult.java
+++ src/main/java/it/unibo/cactus/model/Score/GameResult.java
@@ -1,5 +1,16 @@
 package it.unibo.cactus.model.Score;
 
-public record GameResult() {
+import java.util.Map;
+
+import it.unibo.cactus.model.Players.Player;
+
+public record GameResult(Map<Player, Integer> scores) {
+
+    public Player getWinner() {
+        return scores.entrySet().stream()
+            .min(Map.Entry.comparingByValue())
+            .map(Map.Entry::getKey)
+            .orElseThrow();
+    }
 
 }
