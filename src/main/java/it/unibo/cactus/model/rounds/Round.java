0de3a32834d9782007e1250397aed4ff622267a1
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-03-09 22:12:38 +0100

    Folder rounds changed

---
+++ src/main/java/it/unibo/cactus/model/rounds/Round.java
@@ -0,0 +1,45 @@
+package it.unibo.cactus.model.rounds;
+
+import java.util.List;
+import java.util.Optional;
+import it.unibo.cactus.model.Cards.Card;
+
+/**
+ * Represents a round of the game.
+ * A round manages the available actions for the current player,
+ * the execution of an action, and information about the card drawn
+ * and whether the round is the last one.
+ */
+
+public interface Round {
+
+    /**
+     * Returns the list of actions that can currently be performed
+     * during this round.
+     *
+     * @return a list of available {@link RoundAction}.
+     */
+    List<RoundAction> getAvailableActions();
+
+    /**
+     * Indicates whether this round is the last round of the game.
+     *
+     * @return true if it is the last round, false otherwise.
+     */
+    boolean isLastRound();
+
+    /**
+     * Returns the card drawn during this round, if a card has been drawn.
+     *
+     * @return an {@link Optional} containing the drawn {@link Card},
+     *         or an empty Optional if no card has been drawn.
+     */
+    Optional<Card> getDrawnCard();
+
+    /**
+     * Executes the specified action within the round.
+     *
+     * @param action the {@link RoundAction} to be executed.
+     */
+    void execute(RoundAction action);
+}
