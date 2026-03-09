57f30c9392cc402cda0abcf3831ebbbd8da31baf
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2026-03-09 21:27:18 +0100

    Record PlayerStats.java implemented

--- src/main/java/it/unibo/cactus/model/statistics/PlayerStats.java
+++ src/main/java/it/unibo/cactus/model/statistics/PlayerStats.java
@@ -1,5 +1,7 @@
 package it.unibo.cactus.model.statistics;
 
-public record PlayerStats() {
+import java.util.Map;
+
+public record PlayerStats(int wins, Map<String, Integer> generalRanking, double averageRounds) {
 
 }
