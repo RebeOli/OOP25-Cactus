090b554cdcc0f219e8516ff8a373fb7afa993185
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2026-03-13 12:03:49 +0100

    Class JSonHistoryRepository.java created

---
+++ src/main/java/it/unibo/cactus/model/statistics/JSonHistoryRepository.java
@@ -0,0 +1,21 @@
+package it.unibo.cactus.model.statistics;
+
+import java.util.List;
+
+import it.unibo.cactus.model.score.GameResult;
+
+public class JSonHistoryRepository implements HistoryRepository {
+
+    @Override
+    public void save(GameResult result) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public List<GameResult> loadAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadAll'");
+    }
+
+}
