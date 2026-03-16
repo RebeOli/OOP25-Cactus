8d6f8654c8a8d1c3ec2c18bff1c130a2ef7e31a5
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2026-03-16 11:41:47 +0100

    create and implement DiscardPileTest.java

---
+++ src/test/java/it/unibo/cactus/pile/DiscardPileTest.java
@@ -0,0 +1,56 @@
+package it.unibo.cactus.pile;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.cactus.model.cards.Card;
+import it.unibo.cactus.model.cards.CardImpl;
+import it.unibo.cactus.model.cards.Suit;
+import it.unibo.cactus.model.pile.DiscardPile;
+import it.unibo.cactus.model.pile.DiscardPileImpl;
+
+public class DiscardPileTest {
+    private static final Card CARD_1 = new CardImpl(Suit.BASTONI, 1, 1, null);
+    private static final Card CARD_2 = new CardImpl(Suit.SPADE, 3, 3, null);
+    private static final Card CARD_3 = new CardImpl(Suit.COPPE, 10, 0, null);
+
+    private DiscardPile discardPile;
+
+    @BeforeEach
+    void setUp() {
+        this.discardPile = new DiscardPileImpl();
+    }
+
+    @Test
+    void testDiscard() {
+        assertTrue(this.discardPile.isEmpty());
+        this.discardPile.discard(CARD_1);
+        this.discardPile.discard(CARD_2);
+        assertFalse(this.discardPile.isEmpty());
+        assertEquals(this.discardPile.getTopCard().get(), CARD_2);
+    }
+
+    @Test
+    void testDrainAll() {
+        assertTrue(this.discardPile.isEmpty());
+        this.discardPile.discard(CARD_1);
+        this.discardPile.discard(CARD_2);
+        this.discardPile.discard(CARD_3);
+        assertFalse(this.discardPile.isEmpty());
+        this.discardPile.drainAll();
+        assertTrue(this.discardPile.isEmpty());
+    }
+
+    @Test
+    void testIsEmpty() {
+        assertTrue(this.discardPile.isEmpty());
+        this.discardPile.discard(CARD_1);
+        this.discardPile.discard(CARD_2);
+        this.discardPile.discard(CARD_3);
+        assertFalse(this.discardPile.isEmpty());
+    }
+}
