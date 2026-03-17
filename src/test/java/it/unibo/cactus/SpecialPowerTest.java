7370374c38c37cbd4614b80578faa0256b01db5a
Arianna Mondardini <arianna.mondardini@studio.unibo.it>
2026-03-17 17:18:36 +0100

    Implemented helpler method of a 'fake player' for test

--- src/test/java/it/unibo/cactus/SpecialPowerTest.java
+++ src/test/java/it/unibo/cactus/SpecialPowerTest.java
@@ -1,5 +1,20 @@
 package it.unibo.cactus;
 
-public class SpecialPowerTest {
+import java.util.*;
+import it.unibo.cactus.model.cards.Card;
+import it.unibo.cactus.model.players.Player;
+import it.unibo.cactus.model.players.PlayerHand;
+import it.unibo.cactus.model.players.PlayerHandImpl; 
 
+class SpecialPowerTest {
+    private Player createDummyPlayer(String dummyName, Card... cards) {
+        PlayerHand hand = new PlayerHandImpl(Arrays.asList(cards));
+        return new Player() {
+            private PlayerHand myHand = hand; 
+            public String getName() { return dummyName; }
+            public boolean isHuman() { return true; }
+            public PlayerHand getHand() { return myHand; }
+            public void setHand(PlayerHand h) { this.myHand = h; }
+        };
+    }
 }
