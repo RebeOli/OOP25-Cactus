89011fda5a33aee4ae882d6dcc1a94fe148f205e
Sofia Molari <sofia.molari@studio.unibo.it>
2026-03-21 21:36:06 +0100

    feat: add medium and hard bot strategy
    These strategies are not yet implemented

---
+++ src/main/java/it/unibo/cactus/model/players/HardBotStrategy.java
@@ -0,0 +1,13 @@
+package it.unibo.cactus.model.players;
+
+import it.unibo.cactus.model.rounds.Round;
+import it.unibo.cactus.model.rounds.RoundAction;
+
+public class HardBotStrategy implements BotStrategy {
+    
+    /** {@inheritDoc} */
+    @Override
+    public RoundAction chooseAction(final Round round) {
+        return null;
+    }
+}
---
+++ src/main/java/it/unibo/cactus/model/players/MediumBotStrategy.java
@@ -0,0 +1,12 @@
+package it.unibo.cactus.model.players;
+
+import it.unibo.cactus.model.rounds.Round;
+import it.unibo.cactus.model.rounds.RoundAction;
+
+public class MediumBotStrategy implements BotStrategy {
+        /** {@inheritDoc} */
+    @Override
+    public RoundAction chooseAction(final Round round) {
+        return null;
+    }
+}
