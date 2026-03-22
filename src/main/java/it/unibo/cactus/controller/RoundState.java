21ca5f061f864e9045e7f7e51a890527ab15147b
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-03-22 16:03:33 +0100

    add partial implementation of RoundState

--- src/main/java/it/unibo/cactus/controller/RoundState.java
+++ src/main/java/it/unibo/cactus/controller/RoundState.java
@@ -1,18 +1,30 @@
 package it.unibo.cactus.controller;
 
+import it.unibo.cactus.model.game.Game;
 import it.unibo.cactus.model.rounds.RoundAction;
+import it.unibo.cactus.model.rounds.TurnPhase;
 
 public class RoundState implements ControllerState {
+    final Game game;
+    //final ControllerImpl controller;
+    public RoundState (final Controller controller, final Game game) {
+        //this.controller = controller;
+        this.game = game;
+    }
 
     @Override
     public void handle(RoundAction action) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handleAction'");
+        game.getCurrentRound().execute(action);
+        if (game.getCurrentRound().getAvailableActions().isEmpty()) {
+            game.advancePlayer();
+        }
+        if(game.isFinished()) {
+            //controller.setState(new EndGameState(game));
+        }
     }
 
     @Override
     public void tick() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'tick'");
+        
     }
 }
