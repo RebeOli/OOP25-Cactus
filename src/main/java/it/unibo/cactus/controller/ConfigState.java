740d180a5ecc6c45d2e4cb36c087cdbe4dab3b06
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-03-22 14:23:35 +0100

    add ConfigState class

---
+++ src/main/java/it/unibo/cactus/controller/ConfigState.java
@@ -0,0 +1,19 @@
+package it.unibo.cactus.controller;
+
+import it.unibo.cactus.model.rounds.RoundAction;
+
+public class ConfigState implements ControllerState {
+
+    @Override
+    public void handle(RoundAction action) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
+
+    @Override
+    public void tick() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tick'");
+    }
+    
+}
