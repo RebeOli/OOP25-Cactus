b534d7d5c8d2011f5128086020909e177c3ba7e2
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2026-03-22 16:02:11 +0100

    create EndGameState.java

---
+++ src/main/java/it/unibo/cactus/controller/EndGameState.java
@@ -0,0 +1,19 @@
+package it.unibo.cactus.controller;
+
+import it.unibo.cactus.model.rounds.RoundAction;
+
+public class EndGameState implements ControllerState {
+
+    @Override
+    public void handle(final RoundAction action) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
+
+    @Override
+    public void tick() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tick'");
+    }
+
+}
