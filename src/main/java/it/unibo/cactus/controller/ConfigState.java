fb667c598dd597113826da71a953b21a8a77877f
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-03-22 14:34:11 +0100

    implement ConfigState

--- src/main/java/it/unibo/cactus/controller/ConfigState.java
+++ src/main/java/it/unibo/cactus/controller/ConfigState.java
@@ -1,19 +1,24 @@
 package it.unibo.cactus.controller;
 
+import it.unibo.cactus.model.game.Game;
+import it.unibo.cactus.model.game.GameFactory;
 import it.unibo.cactus.model.rounds.RoundAction;
 
 public class ConfigState implements ControllerState {
 
+    public ConfigState (final String playerName, final Controller controller) { //va cambiato in ControllerImpl quando c'è 
+        final Game game = GameFactory.createGame(playerName); 
+        //controller.setState(new RoundState(controller, game));
+    }
+
     @Override
     public void handle(RoundAction action) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+        // ConfigState does not handle actions
     }
 
     @Override
     public void tick() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'tick'");
+        // ConfigState does not handle ticks
     }
-    
+
 }
