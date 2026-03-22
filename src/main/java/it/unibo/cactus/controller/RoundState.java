1492f854911560e6aa415a17d73775208d549836
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-03-22 14:55:04 +0100

    add RoundState

---
+++ src/main/java/it/unibo/cactus/controller/RoundState.java
@@ -0,0 +1,18 @@
+package it.unibo.cactus.controller;
+
+import it.unibo.cactus.model.rounds.RoundAction;
+
+public class RoundState implements ControllerState {
+
+    @Override
+    public void handle(RoundAction action) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleAction'");
+    }
+
+    @Override
+    public void tick() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tick'");
+    }
+}
