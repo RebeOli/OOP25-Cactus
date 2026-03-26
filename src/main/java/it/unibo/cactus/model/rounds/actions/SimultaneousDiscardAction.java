93e02f231076cd4749ce66ff71564e60628c70cf
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-03-26 16:06:13 +0100

    Partial Implementation of Simultaneous Discard Action.
    We worked together on the logic and implementation.
    
    Co-authored-by: Chiara <chiara.mazzoni12@studio.unibo.it>
    Co-authored-by: Arianna <arianna.mondardini@studio.unibo.it>

--- src/main/java/it/unibo/cactus/model/rounds/actions/SimultaneousDiscardAction.java
+++ src/main/java/it/unibo/cactus/model/rounds/actions/SimultaneousDiscardAction.java
@@ -1,5 +1,35 @@
 package it.unibo.cactus.model.rounds.actions;
 
-public class SimultaneousDiscardAction {
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+
+import it.unibo.cactus.model.cards.Card;
+import it.unibo.cactus.model.game.Game;
+import it.unibo.cactus.model.players.Player;
+import it.unibo.cactus.model.rounds.MutableRound;
+import it.unibo.cactus.model.rounds.RoundAction;
+
+public record SimultaneousDiscardAction(Game game, Player player, int cardIndex) implements RoundAction {
+
+    @Override
+    public void execute() {
+        /*final List<Player> availablePlayers = game.getPlayers().stream()
+            .filter(p -> p.getHand().size() < 6)
+            .toList();*/
+        Card discardCard = player.getHand().getCard(cardIndex);
+        if(discardCard.equals(game.getDiscardPile().getTopCard())) {
+            this.discard(player, discardCard);
+            //player.getHand().removeCard(cardIndex); metodo ari
+        } else {
+            //player.getHand().addCard(game.getDrawPile().draw()); metodo ari
+        }
+    }
+
+    private void discard(final Player player, final Card discardCard) {
+        //Card discardcard = player.getHand().getCard(cardIndex);
+        game.getDiscardPile().discard(discardCard);
+
+    }
     
 }
