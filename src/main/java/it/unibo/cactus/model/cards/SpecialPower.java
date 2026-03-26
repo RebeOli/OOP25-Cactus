efd2a3df2ef489d46bca5cd5f86f5b964784e3c3
Arianna Mondardini <arianna.mondardini@studio.unibo.it>
2026-03-26 11:26:32 +0100

    Remove 'Game' from the method activate

--- src/main/java/it/unibo/cactus/model/cards/SpecialPower.java
+++ src/main/java/it/unibo/cactus/model/cards/SpecialPower.java
@@ -1,7 +1,6 @@
 package it.unibo.cactus.model.cards;
 
 import it.unibo.cactus.model.cards.target.PowerTarget;
-import it.unibo.cactus.model.game.Game;
 import it.unibo.cactus.model.players.Player;
 
 /**
@@ -14,10 +13,9 @@ public interface SpecialPower {
      * Executes the specific logic of the special power associated with the card.
      * The exact behavior depends on the concrete implementation (e.g., Swap, Peek, Reveal).
      *
-     * @param game      the current {@link Game} context, providing access to the overall game state.
      * @param activator the {@link Player} who played the card and triggered the special power.
      * @param target    a {@link PowerTarget} object containing the specific payload/parameters needed for the power.
      * @throws IllegalArgumentException if the provided {@code target} is not of the expected type.
      */
-    void activate(Game game, Player activator, PowerTarget target);
+    void activate(Player activator, PowerTarget target);
 }
