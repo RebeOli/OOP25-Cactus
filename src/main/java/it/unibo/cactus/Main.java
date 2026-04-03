6cbaa2f23a7f56e591c03d1aa342b52ca2eef67b
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-04-03 11:41:18 +0200

    Add css files.
    We worked together on the logic and implementation.
    
    Co-authored-by: Chiara <chiara.mazzoni12@studio.unibo.it>
    Co-authored-by: Arianna <arianna.mondardini@studio.unibo.it>

--- src/main/java/it/unibo/cactus/Main.java
+++ src/main/java/it/unibo/cactus/Main.java
@@ -1,9 +1,18 @@
 package it.unibo.cactus;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Entry point of the Cactus application.
  */
 
-public class Main {
-    //todo
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
 }
