5118144bbcdf272f32140751898c8f1328c81ddf
Arianna Mondardini <arianna.mondardini@studio.unibo.it>
2026-04-20 22:27:32 +0200

    fix checkstyle in class: TableView.java

--- src/main/java/it/unibo/cactus/view/TableView.java
+++ src/main/java/it/unibo/cactus/view/TableView.java
@@ -6,8 +6,19 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+/**
+ * Represents the view of the game table
+ */
 public class TableView extends BorderPane {
 
+    private static final int PADDING_STANDARD = 10;
+    private static final int PADDING_BOTTOM = 80;
+    private static final int PILES_SPACING = 30;
+    private static final int CENTER_SPACING = 20;
+    private static final double CARD_HEIGHT_RATIO = 0.12;
+    private static final double PILE_HEIGHT_RATIO = 0.20;
+    private static final double CARD_WIDTH_RATIO = 0.71;
+    private static final double ZOOMED_CARD_RATIO = 0.25;
     private final PlayerHandView humanHand;
     private final PlayerHandView bot1Hand;
     private final PlayerHandView bot2Hand;
@@ -17,82 +28,141 @@ public class TableView extends BorderPane {
     private final HBox pilesContainer;
     private final CardView zoomedDrawnCard;
 
-    public TableView(String humanName, String bot1Name, String bot2Name, String bot3Name) {
+    /**
+     * Constructs the game table, setting up the players and the layout.
+     *
+     * @param humanName the name of the human player
+     * @param bot1Name the name of the first bot
+     * @param bot2Name the name of the second bot
+     * @param bot3Name the name of the third bot
+     */
+    public TableView(final String humanName, final String bot1Name, final String bot2Name, final String bot3Name) {
         this.getStyleClass().add("gameTable");
-        this.setPadding(new Insets(10, 10, 80, 10));
-
+        this.setPadding(new Insets(PADDING_STANDARD, PADDING_STANDARD, PADDING_BOTTOM, PADDING_STANDARD));
         this.humanHand = new PlayerHandView(humanName, PlayerHandView.Position.BOTTOM);
         this.bot1Hand = new PlayerHandView(bot1Name, PlayerHandView.Position.LEFT);
         this.bot2Hand = new PlayerHandView(bot2Name, PlayerHandView.Position.TOP);
         this.bot3Hand = new PlayerHandView(bot3Name, PlayerHandView.Position.RIGHT);
-
         this.setBottom(humanHand);
         this.setLeft(bot1Hand);
         this.setTop(bot2Hand);
         this.setRight(bot3Hand);
-
         BorderPane.setAlignment(humanHand, Pos.BOTTOM_CENTER);
         BorderPane.setAlignment(bot2Hand, Pos.TOP_CENTER);
         BorderPane.setAlignment(bot1Hand, Pos.CENTER_LEFT);
         BorderPane.setAlignment(bot3Hand, Pos.CENTER_RIGHT);
-        BorderPane.setMargin(humanHand, new Insets(0, 0, 10, 0));
-
-        // mazzi al centro
+        BorderPane.setMargin(humanHand, new Insets(0, 0, PADDING_STANDARD, 0));
         this.drawPile = new DrawPileView();
         this.discardPile = new DiscardPileView();
-
-        this.pilesContainer = new HBox(30);
+        this.pilesContainer = new HBox(PILES_SPACING);
         pilesContainer.setAlignment(Pos.CENTER);
         pilesContainer.getChildren().addAll(drawPile, discardPile);
-
         this.zoomedDrawnCard = new CardView();
         zoomedDrawnCard.setVisible(false);
-
-        VBox centerArea = new VBox(20);
+        VBox centerArea = new VBox(CENTER_SPACING);
         centerArea.setAlignment(Pos.CENTER);
         centerArea.getChildren().addAll(pilesContainer, zoomedDrawnCard);
         this.setCenter(centerArea);
-
         setupResponsiveSizes();
     }
 
     private void setupResponsiveSizes() {
-        javafx.beans.binding.DoubleBinding standardCardHeight = this.heightProperty().multiply(0.12);
+        final javafx.beans.binding.DoubleBinding standardCardHeight = this.heightProperty().multiply(CARD_HEIGHT_RATIO);
         humanHand.bindCardsHeight(standardCardHeight);
         bot1Hand.bindCardsHeight(standardCardHeight);
         bot2Hand.bindCardsHeight(standardCardHeight);
         bot3Hand.bindCardsHeight(standardCardHeight);
-
-        // mazzi al centro
-        javafx.beans.binding.DoubleBinding pileHeight = this.heightProperty().multiply(0.20);
+        final javafx.beans.binding.DoubleBinding pileHeight = this.heightProperty().multiply(PILE_HEIGHT_RATIO);
         drawPile.prefHeightProperty().bind(pileHeight);
         drawPile.maxHeightProperty().bind(pileHeight);
-        drawPile.prefWidthProperty().bind(pileHeight.multiply(0.71));
-        drawPile.maxWidthProperty().bind(pileHeight.multiply(0.71));
-
+        drawPile.prefWidthProperty().bind(pileHeight.multiply(CARD_WIDTH_RATIO));
+        drawPile.maxWidthProperty().bind(pileHeight.multiply(CARD_WIDTH_RATIO));
         discardPile.prefHeightProperty().bind(pileHeight);
         discardPile.maxHeightProperty().bind(pileHeight);
-        discardPile.prefWidthProperty().bind(pileHeight.multiply(0.71));
-        discardPile.maxWidthProperty().bind(pileHeight.multiply(0.71));
+        discardPile.prefWidthProperty().bind(pileHeight.multiply(CARD_WIDTH_RATIO));
+        discardPile.maxWidthProperty().bind(pileHeight.multiply(CARD_WIDTH_RATIO));
 
-        zoomedDrawnCard.bindHeight(this.heightProperty().multiply(0.25));
+        zoomedDrawnCard.bindHeight(this.heightProperty().multiply(ZOOMED_CARD_RATIO));
     }
 
+    /**
+     * Shows the drawn card in the center of the table.
+     *
+     * @param card the card to be shown
+     */
     public void showDrawnCard(it.unibo.cactus.model.cards.Card card) {
         zoomedDrawnCard.setCardData(card);
         zoomedDrawnCard.setFaceUp(true);
         zoomedDrawnCard.setVisible(true);
     }
 
+    /**
+     * Hides the drawn card in the center of the table.
+     */
     public void hideDrawnCard() {
         zoomedDrawnCard.setVisible(false);
     }
 
-    public PlayerHandView getHumanHand() { return humanHand; }
-    public PlayerHandView getBot1Hand() { return bot1Hand; }
-    public PlayerHandView getBot2Hand() { return bot2Hand; }
-    public PlayerHandView getBot3Hand() { return bot3Hand; }
-    public DrawPileView getDrawPile() { return drawPile; }
-    public DiscardPileView getDiscardPile() { return discardPile; }
-    public CardView getZoomedDrawnCard() { return zoomedDrawnCard; }
+    /**
+     * Returns the view of the human player's hand.
+     *
+     * @return the view of the human player's hand
+     */
+    public PlayerHandView getHumanHand() {
+        return humanHand; 
+    }
+
+    /**
+     * Returns the view of the first bot's hand.
+     *
+     * @return the view of the first bot's hand
+     */
+    public PlayerHandView getBot1Hand() {
+        return bot1Hand;
+    }
+
+    /**
+     * Returns the view of the second bot's hand.
+     *
+     * @return the view of the second bot's hand
+     */
+    public PlayerHandView getBot2Hand() {
+        return bot2Hand;
+    }
+
+    /**
+     * Returns the view of the third bot's hand.
+     *
+     * @return the view of the third bot's hand
+     */
+    public PlayerHandView getBot3Hand() {
+        return bot3Hand;
+    }
+
+    /**
+     * Returns the view of the draw pile.
+     *
+     * @return the view of the draw pile
+     */
+    public DrawPileView getDrawPile() {
+        return drawPile;
+    }
+
+    /**
+     * Returns the view of the discard pile.
+     *
+     * @return the view of the discard pile
+     */
+    public DiscardPileView getDiscardPile() {
+        return discardPile;
+    }
+
+    /**
+     * Returns the view of the zoomed drawn card.
+     *
+     * @return the view of the zoomed drawn card
+     */
+    public CardView getZoomedDrawnCard() {
+        return zoomedDrawnCard;
+    }
 }
