46efd91be0fe016114474867e912b773b18b4ca0
Arianna Mondardini <arianna.mondardini@studio.unibo.it>
2026-05-05 18:10:23 +0200

    Trying to fix vertical hands

--- src/main/java/it/unibo/cactus/view/PlayerHandView.java
+++ src/main/java/it/unibo/cactus/view/PlayerHandView.java
@@ -4,6 +4,7 @@ import it.unibo.cactus.model.cards.Card;
 import it.unibo.cactus.model.players.PlayerHand; 
 
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -14,64 +15,52 @@ import javafx.scene.layout.VBox;
  */
 public class PlayerHandView extends VBox {
 
-    /**
-     * Represents the position of the player at the game table.
-     */
     public enum Position {
-        TOP,
-        BOTTOM,
-        LEFT,
-        RIGHT
+        TOP, BOTTOM, LEFT, RIGHT
     }
 
     private static final int MAX_CARDS = 6;
-    private static final double OVERLAP_SPACING = -25.0;
     private static final int STANDARD_SPACING = 10;
     private final Pane cardsContainer;
     private final CardView[] slots;
 
-    /**
-     * Constructs a new PlayerHandView.
-     *
-     * @param playerName the name of the player
-     * @param position the layout position of the player on the table
-     */
     public PlayerHandView(final String playerName, final Position position) {
         this.setAlignment(Pos.CENTER);
         this.setSpacing(STANDARD_SPACING);
+
         final boolean isHumanPlayer = position == Position.BOTTOM;
+        final boolean isSide = position == Position.LEFT || position == Position.RIGHT;
+
         final Label nameLabel = new Label(playerName);
         nameLabel.getStyleClass().add("playerBadge");
         nameLabel.setStyle("-fx-font-size: " + (isHumanPlayer ? "18px" : "12px") + ";");
-        if (position == Position.LEFT || position == Position.RIGHT) {
-            this.cardsContainer = new VBox(OVERLAP_SPACING); 
+
+        if (isSide) {
+            this.cardsContainer = new VBox(STANDARD_SPACING);
+            ((VBox) cardsContainer).setAlignment(Pos.CENTER);
         } else {
-            this.cardsContainer = new HBox(STANDARD_SPACING); 
-        }
-        if (cardsContainer instanceof HBox hbox) {
-            hbox.setAlignment(Pos.CENTER);
-        }
-        if (cardsContainer instanceof VBox vbox) {
-            vbox.setAlignment(Pos.CENTER);
+            this.cardsContainer = new HBox(STANDARD_SPACING);
+            ((HBox) cardsContainer).setAlignment(Pos.CENTER);
         }
 
         this.slots = new CardView[MAX_CARDS];
         for (int i = 0; i < MAX_CARDS; i++) {
-            slots[i] = new CardView(); 
-            slots[i].setVisible(false); 
-            if (position == Position.LEFT || position == Position.RIGHT) {
-                slots[i].setRotate(90); 
+            slots[i] = new CardView();
+            slots[i].setVisible(false);
+
+            if (isSide) {
+                slots[i].setRotate(90);
+                // Group riporta i bounds visivi post-rotazione
+                final Group rotationWrapper = new Group(slots[i]);
+                cardsContainer.getChildren().add(rotationWrapper);
+            } else {
+                cardsContainer.getChildren().add(slots[i]);
             }
-            cardsContainer.getChildren().add(slots[i]);
         }
+
         super.getChildren().addAll(nameLabel, cardsContainer);
     }
 
-    /**
-     * Updates the visual representation of the hand based on the model data.
-     *
-     * @param hand the player's hand model
-     */
     public void updateHand(final PlayerHand hand) {
         for (int i = 0; i < MAX_CARDS; i++) {
             if (i < hand.size()) {
@@ -92,12 +81,6 @@ public class PlayerHandView extends VBox {
         }
     }
 
-    /**
-     * Retrieves the card view slot at the specified index.
-     *
-     * @param index the index of the slot to retrieve
-     * @return the card view slot, or null if the index is out of bounds
-     */
     public CardView getSlot(final int index) {
         if (index >= 0 && index < MAX_CARDS) {
             return slots[index];
@@ -111,11 +94,6 @@ public class PlayerHandView extends VBox {
         });
     }
 
-    /**
-     * Binds the height of all cards in the hand to an external property for responsive sizing.
-     *
-     * @param heightBinding the property to bind the height to
-     */
     public void bindCardsHeight(final javafx.beans.binding.DoubleBinding heightBinding) {
         for (final CardView slot : slots) {
             slot.bindHeight(heightBinding);
